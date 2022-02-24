c9907169daefcdf444fbb729bab747d03a9c874c
Mueid <mrmuyed579@gmail.com>
2022-02-24 19:09:43 +0600

    second section done

--- js/app.js
+++ js/app.js
@@ -10,3 +10,38 @@ const clicked = () =>{
     .then(data => displayMeals(data.meals));
 }
 
+const displayMeals = meals => {
+    const searchResult = document.getElementById("search-results");
+   /*  meals.forEach(meal => {
+        console.log(meal);
+        const div = document.createElement('div');
+     div.classList.add('col');
+     div.innerHTML = `
+              <div class="card h-100">
+                  <img src="..." class="card-img-top" alt="...">
+                  <div class="card-body">
+                   <h5 class="card-title">Card title</h5>
+                   <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  </div>
+              </div>
+               
+     `;
+     searchResult.appendChild(div);
+    })  */
+   for(const meal of meals){
+       console.log(meal);
+       const div = document.createElement('div');
+     div.classList.add('col');
+     div.innerHTML = `
+              <div class="card h-100">
+                  <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                  <div class="card-body">
+                   <h5 class="card-title">${meal.strMeal}</h5>
+                   <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  </div>
+              </div>
+               
+     `;
+     searchResult.appendChild(div);
+   }
+ }
